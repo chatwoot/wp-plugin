3c9f63e4b3519f1db049ffa2471bfefa8a941311
Pranav Raj S <pranav@chatwoot.com>
2021-09-08 14:08:29 +0530

    chore: Add options to customize the design of the widget

--- js/chatwoot.js
+++ js/chatwoot.js
@@ -1,12 +1,17 @@
+window.chatwootSettings = {
+  locale: chatwoot_widget_locale,
+  type: chatwoot_widget_type,
+  position: chatwoot_widget_position,
+  launcherTitle: chatwoot_launcher_text,
+};
+
 (function(d,t) {
-    var BASE_URL= chatwoot_url;
-    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=BASE_URL+"/packs/js/sdk.js";
-    s.parentNode.insertBefore(g,s);
-    g.onload=function(){
-      window.chatwootSDK.run({
-        websiteToken: chatwoot_token,
-        baseUrl: BASE_URL
-      })
-    }
-  })(document,"script");
+  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.async=!0;
+  g.defer=!0;
+  g.src=chatwoot_url+"/packs/js/sdk.js";
+  s.parentNode.insertBefore(g,s);
+  g.onload=function(){
+    window.chatwootSDK.run({ websiteToken: chatwoot_token, baseUrl: chatwoot_url })
+  }
+})(document,"script");
